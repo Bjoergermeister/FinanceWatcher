91c7fc026545c8816adf359ef0de4e162a903b29
Bjoergermeister <bjarnesvensson@web.de>
2024-01-03 13:13:25 +0000

    Update required inputs and initial values in bill and position forms.

--- app/forms/bills.py
+++ app/forms/bills.py
@@ -1,11 +1,13 @@
+import datetime
+
 from django import forms
 
 from ..models.Bill import Bill
 
 class CreateBillForm(forms.ModelForm):
     user = forms.UUIDField(widget=forms.HiddenInput())
-    date = forms.DateField(label="Datum")
-    total = forms.DecimalField(label="Summe")
+    date = forms.DateField(label="Datum", initial=datetime.date.today(), widget=forms.DateInput(attrs={ 'type': 'date'}))
+    total = forms.DecimalField(widget=forms.HiddenInput(), initial=0.0)
     description = forms.CharField(label="Beschreibung (optional)", required=False, widget=forms.Textarea(attrs={'rows': 3}))
     receipt = forms.FileField(label="Kassenzettel (optional)", required=False)
 
--- app/forms/positions.py
+++ app/forms/positions.py
@@ -3,10 +3,10 @@ from django import forms
 from ..models.Position import Position
 
 class CreatePositionForm(forms.ModelForm):
-    name = forms.CharField(label="")
+    name = forms.CharField(label="", min_length=1, widget=forms.TextInput(attrs={ 'required': True }))
     price = forms.DecimalField(label="", decimal_places=2, initial=0.0)
     quantity = forms.DecimalField(label="", decimal_places=2, initial=1.0)
-    note = forms.CharField(widget=forms.HiddenInput())
+    note = forms.CharField(label="", required=False, widget=forms.HiddenInput())
 
     class Meta:
         model = Position
