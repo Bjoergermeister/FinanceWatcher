59baa97a7efff6b5c37e6cb656ff9cd995be34f6
Bjoergermeister <bjarnesvensson@web.de>
2025-03-11 18:34:48 +0100

    Bugfix: Name for bill was not initialized correctly

--- app/forms/bills.py
+++ app/forms/bills.py
@@ -5,20 +5,24 @@ from django import forms
 from ..models.Bill import Bill
 
 
-def get_initial_name():
-    bill_count = Bill.objects.count()
-    return f"Rechnung #{bill_count + 1}"
-
-
 class CreateBillForm(forms.ModelForm):
     id = forms.IntegerField(required=False, widget=forms.HiddenInput())
-    name = forms.CharField(label="Name", initial=get_initial_name)
+    name = forms.CharField(label="Name")
     user = forms.UUIDField(widget=forms.HiddenInput())
-    date = forms.DateField(label="Datum", initial=datetime.date.today(), widget=forms.DateInput(attrs={'type': 'date'}))
+    date = forms.DateField(label="Datum", widget=forms.DateInput(attrs={'type': 'date'}))
     total = forms.DecimalField(widget=forms.HiddenInput(), initial=0.0)
     description = forms.CharField(label="Beschreibung (optional)", required=False, widget=forms.Textarea(attrs={'rows': 3}))
     receipt = forms.FileField(label="Kassenzettel (optional)", required=False)
     paid = forms.BooleanField(label="Bezahlt", required=False)
+
+    def __init__(self, *args, **kwargs):
+        super(CreateBillForm, self).__init__(*args, **kwargs)
+
+        if self.is_bound is False:
+            self.fields["date"].initial = datetime.date.today()
+
+            bill_count = Bill.objects.order_by("pk").last().pk
+            self.fields["name"].initial = f"Rechnung #{bill_count + 1}"
 
     class Meta:
         model = Bill
