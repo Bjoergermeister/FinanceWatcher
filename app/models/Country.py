a3ed3f956dccd0c0725c79331af9d59cda3b3233
Bjarne Svensson <bjarnesvensson@web.de>
2026-01-04 15:16:36 +0000

    Add country flags to the address table

--- app/models/Country.py
+++ app/models/Country.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
 
+import os
+
 from django.db import models
+from django.templatetags.static import static
 
 class Country(models.Model):
     name = models.CharField(max_length=100)
@@ -9,6 +12,10 @@ class Country(models.Model):
 
     def __str__(self: Country) -> str:
         return self.name
+
+    def get_flag_url(self: Country):
+        path = os.path.join("images", "countries", f"{self.name.lower()}.jpg")
+        return static(path)
 
     class Meta:
         db_table = 'Country'
