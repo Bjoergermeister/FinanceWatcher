960d827b8c352b24d1e1cebabc03041c135444cf
Bjarne Svensson <bjarnesvensson@web.de>
2026-06-19 22:41:13 +0000

    Increase decimal places for position price and quantity from 2 to 3

--- app/models/Position.py
+++ app/models/Position.py
@@ -8,8 +8,8 @@ class Position(models.Model):
     bill = models.ForeignKey(Bill, db_column="bill", on_delete=models.CASCADE, related_name="positions")
     group = models.ForeignKey(Group, db_column="group", on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(db_column="name", max_length=128)
-    price = models.DecimalField(db_column="price", max_digits=13, decimal_places=2)
-    quantity = models.DecimalField(db_column="quantity", max_digits=13, decimal_places=2)
+    price = models.DecimalField(db_column="price", max_digits=13, decimal_places=3)
+    quantity = models.DecimalField(db_column="quantity", max_digits=13, decimal_places=3)
     note = models.TextField(db_column="note", blank=True, null=True)
 
     class Meta:
