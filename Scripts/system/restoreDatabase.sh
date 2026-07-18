3cf7acd05525a3a21b06d441600dc95e76d94dec
Bjarne Svensson <bjarnesvensson@web.de>
2026-07-18 15:20:06 +0200

    The restoreDatabase script takes care of recreating the database and the owner role

--- Scripts/system/restoreDatabase.sh
+++ Scripts/system/restoreDatabase.sh
@@ -9,4 +9,12 @@ else
 fi
 
 docker cp ./db_backup/db_backup_$timestamp.dump $DB_HOST:/tmp/db_backup_$timestamp.dump
-docker exec -t financewatcher-database pg_restore --format=custom --dbname=$dbname --user=$DB_USER /tmp/db_backup_$timestamp.dump
+
+docker exec $DB_HOST psql -U admin --dbname postgres -c "DROP ROLE IF EXISTS $DB_USER;"
+docker exec $DB_HOST psql -U admin --dbname postgres -c "CREATE ROLE $DB_USER WITH LOGIN BYPASSRLS PASSWORD '$DB_PASSWORD';"
+
+docker exec $DB_HOST dropdb -U admin --if-exists --maintenance-db=postgres $DB_NAME
+docker exec $DB_HOST createdb -U admin --maintenance-db=postgres $DB_NAME --owner $DB_USER
+
+docker exec $DB_HOST pg_restore --format=custom --dbname=$dbname --user=$DB_USER /tmp/db_backup_$timestamp.dump
+docker exec $DB_HOST rm -rf /tmp/db_backup_$timestamp.dump0
